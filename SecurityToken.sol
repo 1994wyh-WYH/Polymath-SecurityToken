e410008695c47363bdaf49600589b7e8210f07b0
1994wyh-WYH <33205420+1994wyh-WYH@users.noreply.github.com>
2018-07-05 13:46:59 -0500

    more solid implementations

--- SecurityToken.sol
+++ SecurityToken.sol
@@ -1368,6 +1368,22 @@ interface ITokenBurner {
 
 }
 
+contract TokenBurner is ITokenBurner {
+
+    address public securityToken;
+
+    constructor (address _securityToken) public {
+        securityToken = _securityToken;
+    }
+
+    function burn(address /* _burner */, uint256 /* _value */) public view returns(bool) {
+        require(msg.sender == securityToken);
+        // Add the schematics for the burner( token holder) that backing the burning of the securities
+        return true;
+    }
+
+}
+
 /**
  * @title Transfer Manager module for core transfer validation functionality
  */
